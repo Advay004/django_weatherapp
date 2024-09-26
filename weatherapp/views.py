17af17f4e7abf235e48d81f9ec5b2ac6672dde95
Advay004 <136166281+Advay004@users.noreply.github.com>
2024-09-26 21:57:26 +0300

    added cide

---
+++ weatherapp/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+import datetime
+import requests
+
+# Create your views here.
+
+def index(request):
+    
+    API_KEY='88c35d42aa864d5770b737fa5cac7b64'
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+
+
+    if request.method=="POST":
+        city1=request.POST['city1']
+        city2=request.POST.get('city2',None)
+        weatherdata1=fetch_weather_and_forecast(city1,API_KEY,current_weather_url)
+        if city2:
+            weatherdata2=fetch_weather_and_forecast(city2,API_KEY,current_weather_url)
+        else:
+            weatherdata2=None
+            
+        context={
+            "weatherdata1":weatherdata1,
+            
+            "weatherdata2":weatherdata2,
+            
+        }
+        return render(request,'index.html',context)
+
+    else:
+        return render(request,'index.html')
+def fetch_weather_and_forecast(city, api_key, current_weather_url):
+    try:
+        # Fetch current weather data
+        response = requests.get(current_weather_url.format(city, api_key)).json()
+
+        # Check if the response contains the expected data
+        if 'coord' not in response:
+            print(f"Error: Unable to fetch coordinates for city: {city}")
+            return None, None
+
+        lat = response['coord']['lat']
+        lon = response['coord']['lon']
+
+    
+
+        weatherdata = {
+            "city": city,
+            "temperature": round(response['main']['temp'] - 273.15, 2),
+            "description": response['weather'][0]['description'],
+            "icon": response['weather'][0]['icon']
+        }
+
+
+        return weatherdata
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None, None
+
+
+
