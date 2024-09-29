4ed440dc5c80c76e9690d551fde8ec42055cdf3d
Advay004 <136166281+Advay004@users.noreply.github.com>
2024-09-29 12:13:41 +0300

    chaged code

--- weather/settings.py
+++ weather/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4=bv_^i&ev_d5!a#r-_m7gnd3)%@mx#h83j1bnm#@@yp_@93#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
