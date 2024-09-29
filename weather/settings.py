9b4d937fede2e1d18667d15873a377f45b8ad122
Advay004 <136166281+Advay004@users.noreply.github.com>
2024-09-29 14:16:03 +0300

    done some modification

--- weather/settings.py
+++ weather/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from decouple import config
 
 API_KEY = config('API_KEY')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
