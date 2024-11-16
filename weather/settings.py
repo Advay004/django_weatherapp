00bc5e7f01656534e790ea14749e6ab97b4e6fd9
Advay004 <136166281+Advay004@users.noreply.github.com>
2024-11-16 16:53:43 +0300

    added

--- weather/settings.py
+++ weather/settings.py
@@ -129,3 +129,11 @@ API_KEY = config('API_KEY')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+# Update database configuration from $DATABASE_URL environment variable (if defined)
+import dj_database_url
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
